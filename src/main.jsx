30cbf7831b7a0784c1084c83eb388486da4736bd
Tanayay <tanaysadhwani@gmail.com>
2026-07-08 00:52:07 -0700

    Import feedback fix styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 import './recs.css';
+import './feedback-fixes.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
