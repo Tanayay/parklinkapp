6099cf689821eca794104bcd4f9b2f1b7e77115a
Tanayay <tanaysadhwani@gmail.com>
2026-07-08 02:39:03 -0700

    Restore full ParkLink app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppSafe.jsx';
+import App from './App.jsx';
 import './styles.css';
 import './recs.css';
 import './feedback-fixes.css';
