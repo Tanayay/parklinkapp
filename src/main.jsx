46dbf8ef6f5bab1924551ffef7afdff2966f7ee7
Tanayay <tanaysadhwani@gmail.com>
2026-07-10 00:48:56 -0700

    Use polished AI place picker app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppParkFlowPlacePick.jsx';
+import App from './AppParkFlowPolished.jsx';
 import './styles.css';
 import './recs.css';
 import './feedback-fixes.css';
 import './parklink-map-fixes.css';
 import './parklink-final-fixes.css';
+import './parklink-polished-picker.css';
 
 try {
   const savedUser = JSON.parse(localStorage.getItem('parklink-user') || 'null');
